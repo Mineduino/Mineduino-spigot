1e730eb59d1f7dd35bf791fe46c9b05bf3b84cbd
Adam Raźniewski <adam@razniewski.eu>
2018-11-03 15:49:08 +0100

    mqtt client first schetch

--- src/main/java/com/mineduino/MineduinoSpigotPlugin/MineduinoSpigotPlugin.java
+++ src/main/java/com/mineduino/MineduinoSpigotPlugin/MineduinoSpigotPlugin.java
@@ -3,17 +3,54 @@ package com.mineduino.MineduinoSpigotPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.mineduino.MineduinoSpigotPlugin.Handlers.SignalHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MineduinoSpigotPlugin extends JavaPlugin{
 	
 	public static MineduinoSpigotPlugin instance;
+        public static MqttClient client;
 	
 	@Override
 	public void onEnable() {
 		instance = this;
 		
 		instance.getServer().getPluginManager().registerEvents(new SignalHandler(), instance);
-		
+            try {
+                client = new MqttClient("tcp://dev.mineduino.com:1883", MqttClient.generateClientId(), new MemoryPersistence());
+                client.connect();
+                client.setCallback(new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable thrwbl) {
+                        
+                    }
+
+                    @Override
+                    public void messageArrived(String string, MqttMessage mm) throws Exception {
+                        getLogger().info(mm.toString());
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken imdt) {
+                        
+                    }
+                    
+                });
+            } catch (MqttException ex) {
+                Logger.getLogger(MineduinoSpigotPlugin.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                client.subscribe("test");
+            } catch (MqttException ex) {
+                Logger.getLogger(MineduinoSpigotPlugin.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                        
 		
 	}
 	//TEST
